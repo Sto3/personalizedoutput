08b2211102b9888ff6170cf76c6c5567954260b1
Sto3 <matthew_riley10@outlook.com>
2026-01-20 18:42:45 -0500

    Deprecate V5Config in favor of RediConfig
    V5Config now just aliases to RediConfig for backward compatibility.
    All new code should use RediConfig directly.

--- ios-app/Redi/Services/V5/V5Config.swift
+++ ios-app/Redi/Services/V5/V5Config.swift
@@ -1,25 +1,13 @@
+/**
+ * V5Config.swift
+ *
+ * DEPRECATED - Use RediConfig.swift instead.
+ * This file exists only for backward compatibility.
+ * All new code should use RediConfig directly.
+ */
+
 import Foundation
 import AVFoundation
 
-struct V5Config {
-    static var serverURL: URL {
-        return URL(string: "wss://redialways.com/ws/redi?v=7")!
-    }
-
-    struct Audio {
-        static let sampleRate: Double = 24000
-        static let channels: AVAudioChannelCount = 1
-        static let bytesPerSample = 2
-        static let bitsPerSample = 16
-        static let recordingBufferSize: AVAudioFrameCount = 4800
-        static let playbackChunkSize = 9600
-        static let minPlaybackBuffer = 1200
-    }
-
-    struct Camera {
-        static let staticFrameInterval: TimeInterval = 5.0
-        static let motionFrameInterval: TimeInterval = 0.25
-        static let maxDimension: CGFloat = 768  // Higher for better OCR
-        static let compressionQuality: CGFloat = 0.85  // Higher quality
-    }
-}
+/// Deprecated: Use RediConfig instead
+typealias V5Config = RediConfig
